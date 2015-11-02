b3bb77fa4b1868b32f0d5c8fd15f25a743a7fc31
unknown <x15g019@PC504N023.chiba-fjb.ac.jp>
2015-11-02 15:09:20 +0900

    mok

---
+++ src/jp/ac/chibafjb/x15g019/kd101/L106.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g019.kd101;
+
+public class L106 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.print("(");
+		System.out.print("⌒");
+		System.out.print(",");
+		System.out.print("_");
+		System.out.print("ゝ");
+		System.out.print("⌒");
+		System.out.println(")");
+	}
+
+}
