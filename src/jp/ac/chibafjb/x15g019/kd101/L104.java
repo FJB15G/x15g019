6b2aae9d13b96c89983d68a64976e32eb64b0c7a
unknown <x15g019@PC504N023.chiba-fjb.ac.jp>
2015-11-02 14:49:00 +0900

    mo

---
+++ src/jp/ac/chibafjb/x15g019/kd101/L104.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g019.kd101;
+
+public class L104 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+	}
+
+}
