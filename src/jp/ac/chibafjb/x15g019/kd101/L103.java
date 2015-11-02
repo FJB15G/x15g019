5e4ed9d45fadfb8a1d5e47ed5d8adcbe3cb0d1a0
unknown <x15g019@PC504N023.chiba-fjb.ac.jp>
2015-11-02 14:46:35 +0900

    ｍ２

---
+++ src/jp/ac/chibafjb/x15g019/kd101/L103.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g019.kd101;
+
+public class L103 {
+
+	public static void main(String[] args) {
+		System.out.println("1 船橋 太郎 2 船橋 太郎 3 船橋 太郎 4 船橋 太郎 5 船橋 太郎 6 船橋 太郎 7 船橋 太郎 8 船橋 太郎 9 船橋 太郎 10 船橋 太郎");
+
+
+	}
+
+}
