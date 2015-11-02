3bd487efd78d3638a39e121c5dc1035d32815fae
unknown <x15g019@PC504N023.chiba-fjb.ac.jp>
2015-11-02 14:25:31 +0900

    課題tes

---
+++ src/jp/ac/chibafjb/x15g019/kd101/L101.java
@@ -0,0 +1,9 @@
+package jp.ac.chibafjb.x15g019.kd101;
+
+public class L101 {
+	public static void main(String[] args) {
+	System.out.println ("船橋情報ビジネス専門学校");	
+	System.out.println("047-425-1111");
+	
+}
+}
