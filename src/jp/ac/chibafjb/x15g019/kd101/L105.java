fb152cfcfb4c95476f2165579d764ecdc386da82
unknown <x15g019@PC504N023.chiba-fjb.ac.jp>
2015-11-02 14:58:56 +0900

    TTBN

---
+++ src/jp/ac/chibafjb/x15g019/kd101/L105.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g019.kd101;
+
+public class L105 {
+
+	public static void main(String[] args) {
+		System.out.println("橘樹 松左衛門 (たちばな まつざえもん)");
+		System.out.println("フィンランド人の母とフランス人の父を両親に持つハーフの女の子");
+		System.out.println("人の家に火を放つのが趣味");
+		
+	}
+
+}
