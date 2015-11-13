b6c5eaa01ea769eca4b53556c3c3a5a6f283bfce
unknown <x15g019@PC505N023.chiba-fjb.ac.jp>
2015-11-13 11:17:28 +0900

    no message

---
+++ src/jp/ac/chibafjb/x15g019/kd101/L142.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g019.kd101;
+
+import java.util.Scanner;
+
+public class L142 {
+
+	public static void main(String[] args) {
+		
+		Scanner sin = new Scanner(System.in); 
+		String s = sin.next();                
+		sin.close();
+
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g019/kd101/L143.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g019.kd101;
+
+public class L143 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
